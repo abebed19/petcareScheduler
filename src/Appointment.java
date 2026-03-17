3f6e82c01b6ecc4502efa0b26b2830b5b72f0222
abebed19 <abebed19@gmail.com>
2026-03-17 14:58:50 +0300

    Appointment class

---
+++ src/Appointment.java
@@ -0,0 +1,23 @@
+import java.time.LocalDateTime;
+
+public class Appointment {
+    private String appointmentType;
+    private LocalDateTime dateTime;
+    private String note;
+    public Appointment(String appointmentType, String note) {
+        this.appointmentType = appointmentType;
+        this.dateTime = LocalDateTime.now();
+        this.note = note;
+    }
+
+    public String getAppointmentType() { return this.appointmentType; }
+    public LocalDateTime getDateTime() { return this.dateTime; }
+    public String getNote() { return this.note; }
+    public void setAppointmentType(String appointmentType) { this.appointmentType = appointmentType; }
+    public void setDateTime(LocalDateTime dateTime) { this.dateTime = dateTime; }
+    public void setNote(String note) { this.note = note; }
+    @Override
+    public String toString() {
+        return String.format("%s - %s - %s", this.appointmentType, this.dateTime, this.note);
+    }
+}
