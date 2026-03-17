3e872d9f916b8b0d037b4fb12c779f2071c9210a
abebed19 <abebed19@gmail.com>
2026-03-17 15:36:13 +0300

    Pet getters, Setters and toString methods

--- src/Pet.java
+++ src/Pet.java
@@ -26,6 +26,31 @@ public class Pet {
         this.appointments.add(appointment);
     }
 
+    public String getId() { return this.Id; }
+    public String getName() { return this.name; }
+    public String getSpecies() { return this.species; }
+    public int getAge() { return this.age; }
+    public String getOwnerName() { return this.ownerName; }
+    public String getContactInfo() { return this.contactInfo; }
+    public LocalDate getRegistrationDate() { return this.registrationDate; }
+    public List<Appointment> getAppointments() { return this.appointments; }
+    public void setId(String id) { this.Id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setSpecies(String species) { this.species = species; }
+    public void setAge(int age) { this.age = age; }
+    public void setOwnerName(String ownerName) { this.ownerName = ownerName; }
+    public void setContactInfo(String contactInfo) { this.contactInfo = contactInfo; }
+
+    @Override
+    public String toString() {
+        return "Id : " + this.Id +
+                "\nName : " + this.name +
+                "\nSpecies : " + this.species +
+                "\nAge : " + this.age +
+                "\nOwnerName : " + this.ownerName +
+                "\nContact Info : " + this.contactInfo+
+                "\nRegistration Date : " + this.registrationDate;
 
 
+    }
 }
